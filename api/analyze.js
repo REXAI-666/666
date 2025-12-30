9011ad88fbd15e107303385e414022b27cbfce20
REXAI-666 <kuchnhinikalta@gmail.com>
2025-12-30 19:23:20 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     }
 
     const geminiRes = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=" +
+      ""https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
         apiKey,
       {
         method: "POST",
