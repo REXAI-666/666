d851edc4c5a171fc3b1c1905997045479bcff771
REXAI-666 <kuchnhinikalta@gmail.com>
2025-12-30 17:45:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+body {
+  background:#0f0f0f;
+  color:#fff;
+  font-family:sans-serif;
+  margin:0;
+  padding:10px;
+}
 
+.app {
+  max-width:420px;
+  margin:auto;
+}
+
+h2 {
+  text-align:center;
+}
+
+label {
+  margin-top:12px;
+  display:block;
+}
+
+select,input,textarea,button {
+  width:100%;
+  padding:10px;
+  margin-top:6px;
+  border-radius:8px;
+  border:none;
+}
+
+button {
+  background:#4f46e5;
+  color:white;
+  font-size:16px;
+  margin-top:15px;
+}
+
+textarea {
+  margin-top:15px;
+  height:200px;
+}
+
+.check {
+  display:flex;
+  justify-content:space-between;
+  margin-top:10px;
+}
